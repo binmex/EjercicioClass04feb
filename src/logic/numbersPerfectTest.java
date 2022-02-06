dd4dad5a5aa1a28297332a9dc12c3390592e8339
binmex <robin019@hotmail.com>
2022-02-05 19:54:01 -0500

    sda

--- src/logic/numbersPerfectTest.java
+++ src/logic/numbersPerfectTest.java
@@ -1,13 +1,12 @@
 package logic;
 
-import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class numbersPerfectTest {
     @Test
     void findPerfect(){
-        System.out.println(numbersPerfect.finPerfects(100,200));
+        System.out.println(numbersPerfect.finPerfects(100,2000));
     }
 
 }
--- src/run/Runner.java
+++ src/run/Runner.java
@@ -7,11 +7,15 @@ public class Runner {
     public static void main(String[] args) {
         long timeBegin = System.currentTimeMillis();
 
-        thredFriends frOne = new thredFriends(100,5000);
-        thredFriends frTwo = new thredFriends(5000,10000);
+        thredFriends frOne = new thredFriends(100,2500);
+        thredFriends frTwo = new thredFriends(2501,5000);
+        thredFriends frThree = new thredFriends(5001,7500);
+        thredFriends frFour = new thredFriends(7501,10000);
 
         Thread thOne = new Thread(frOne);
         Thread thtwo = new Thread(frTwo);
+        Thread thThree = new Thread(frThree);
+        Thread thFour = new Thread(frFour);
 
         thOne.start();
         //new Thread(frOne).start();
@@ -23,15 +27,32 @@ public class Runner {
         thtwo.start();
         //new Thread(frTwo).start();
         try {
-            thtwo.join();
+            thtwo.join();//parecido al stop pero no lo para sino espera
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thThree.start();
+        try {
+            thThree.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thFour.start();
+        try {
+            thFour.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         System.out.println(frOne.getFirends());
         System.out.println(frTwo.getFirends());
+        System.out.println(frThree.getFirends());
+        System.out.println(frFour.getFirends());
 
         //System.out.println(numbersPerfect.finPerfects(200,10000));
-        System.out.println((double) (System.currentTimeMillis()-timeBegin)/1000+ "seg");
+        System.out.println((double) (System.currentTimeMillis()-timeBegin)/1000+ " seg");
+        System.out.println((double) ((System.currentTimeMillis()-timeBegin)/1000)/60+ " min");
     }
 }
